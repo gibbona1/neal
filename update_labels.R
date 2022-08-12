61ccc9e3f21de4c6846f4119930e41b679d51b04
gibbona1 <33295389+gibbona1@users.noreply.github.com>
2022-08-12 13:25:28 +0100

    wavfile cleanup

--- update_labels.R
+++ update_labels.R
@@ -1,9 +1,13 @@
-labs_df <- read.csv("data-2022-06-13.csv")
-
+library(stringr)
+labs_df <- read.csv("C:/Users/Anthony/Downloads/data-2022-08-12 (2).csv")
+#labs_df$date_time <- stringr::str_replace_all(lab_df$date_time, "/", "-")
+min(labs_df$date_time)
+tail(labs_df, 10)
 if("X" %in% colnames(labs_df))
   labs_df <- subset(labs_df, select = -c(X))
 
 labfolders <- gsub('www/', '', list.dirs('www')[-1])
+labfolders <- labfolders[-which(labfolders=="JS")]
 for(lab in labfolders){
   lab_pre <- sapply(labs_df$labeler, function(x) str_split(x, '@')[[1]][1])
   if(lab == 'tmp')
@@ -18,6 +22,6 @@ for(lab in labfolders){
   if(nrow(save_df) == 0)
     save_df <- lab_df
   else
-    save_df <- rbind(save_df, lab_df[lab_df$date_time > max(save_df$date_time),])
+    save_df <- lab_df #rbind(save_df, lab_df)#[lab_df$date_time > max(save_df$date_time),])
   write.csv(save_df, fname, row.names = FALSE)
 }
