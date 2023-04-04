bddb9ad62d8fe50ba8a7749ba089c3cfd97ac533
gibbona1 <33295389+gibbona1@users.noreply.github.com>
2023-04-04 22:37:30 +0100

    more testing

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -9,3 +9,35 @@ test_that("{shinytest2} recording: neal-start", {
   app$click("end_labelling")
   app$expect_values()
 })
+
+
+test_that("{shinytest2} recording: neal-labelling", {
+  app <- AppDriver$new(name = "neal-labelling", height = 569, width = 979)
+  app$set_inputs(`side-panel` = FALSE)
+  app$wait_for_value(input = "start_labelling", timeout = 2000)
+  app$click("start_labelling")
+  app$click("save_points")
+  app$click("remove_points")
+  app$wait_for_value(input = "end_labelling", timeout = 2000)
+  app$click("end_labelling")
+  app$expect_values()
+  app$expect_values()
+})
+
+
+test_that("{shinytest2} recording: neal-categories", {
+  app <- AppDriver$new(name = "neal-categories", height = 569, width = 979)
+  app$wait_for_value(input = "start_labelling", timeout = 2000)
+  app$click("start_labelling")
+  app$set_inputs(otherCategory = "abc")
+  app$click("addCategory")
+  app$click("remCategory")
+  app$set_inputs(`side-panel` = TRUE)
+  app$set_inputs(sidebarItemExpanded = "Configuration")
+  app$set_inputs(species_list = "Carnsore")
+  app$set_inputs(bto_codes = TRUE)
+  app$set_inputs(sidebarItemExpanded = character(0))
+  app$wait_for_value(input = "end_labelling", timeout = 2000)
+  app$click("end_labelling")
+  app$expect_values()
+})
