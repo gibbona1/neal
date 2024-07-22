485cf1e3abba0c2258ec39034e199e78055b36d2
gibbona1 <33295389+gibbona1@users.noreply.github.com>
2024-07-22 18:49:39 +0100

    skip shiny test in check
    may need to return to this

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -1,6 +1,7 @@
 library(shinytest2)
 
 test_that("{shinytest2} recording: test1", {
+  skip_if(is_checking() || !interactive())
   app <- AppDriver$new(variant = platform_variant(), name = "test1", height = 911, 
       width = 1619)
   app$set_inputs(label_points = "Bar-tailed Godwit")
